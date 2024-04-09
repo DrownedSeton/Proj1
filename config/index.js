3c9515ed77eae2edf81a35554a8786b60c3b89f4
DrownedSeton <vip.as.as.200@mail.ru>
2024-04-09 18:55:09 +0300

    gg

--- config/index.js
+++ config/index.js
@@ -4,13 +4,13 @@ const fs = require('fs');
 const config = {
     db: {
         mysql : {
-            host: 'localhost',
+            host: 'db-mysql-fra1-51752-do-user-9208055-0.c.db.ondigitalocean.com',
             user: 'user38', // замените на своего пользователя
             database: 'db38', // можете заменить 'appdb' на свое название базы данных
             password: 'AVNS_vHOnKAWv5W82JkrFdF3', // замените это на пароль от своего пользователя
             port: 25060, // порт базы данных
             ssl: {
-                ca: fs.readFileSync('C:/Git/Proj1/config/ca-certificate-test.crt'), // Путь к файлу ca.crt
+                ca: fs.readFileSync('C:/novik/Proj1/config/ca-certificate-test.crt'), // Путь к файлу ca.crt
             }
         },
     },
